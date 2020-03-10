02c4f7cd437883ffeef4635a6705390442cfafe4
CWC-88 <29718763+CWC-88@users.noreply.github.com>
2020-03-09 19:18:07 -0500

    asdifjasdfasdf

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,40 @@
-import React from 'react';
-import './App.css';
 
-const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+import React, { useEffect, useState } from "react";
+import "../../starwars/src/App.css";
+import ReactDOM from "react-dom";
+import styled from 'styled-components'
+const TextStlyle = styled.h5
+`color:red
 
+`
+
+;
+
+
+
+function App() {
+  const [characters, setCharacters] = useState([]);
+  useEffect(() => {
+    fetch("https://swapi.co/api/people/")
+      .then(response => response.json())
+      .then(({ results }) => {
+        setCharacters(results); 
+      });
+  }, []); 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      {characters.map((character, index) => (
+        <div key={index} className="card">
+          <TextStlyle>{character.name}</TextStlyle>
+        </div>
+      ))}
     </div>
   );
 }
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
 
 export default App;
